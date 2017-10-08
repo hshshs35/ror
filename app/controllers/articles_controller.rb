4abccc8642d3311bc075f2c64f054dd378506ebe
Shawn Huang <huangsha4248@gmail.com>
2017-10-08 21:49:59 +0000

    create the show function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,17 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(articles_params)
-        @article.save
-        redierct_to articles_show(@article)
+        
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private
